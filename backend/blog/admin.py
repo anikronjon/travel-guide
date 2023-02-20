d78e46503258738e29a2f5e95eb7c709eca321ee
Anik Ronjon <anik35-497@diu.edu.bd>
2023-02-21 00:37:24 +0600

    blog models and admin created

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    list_editable = ['status']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+
--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,4 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here
+
+# Published Manager
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', _('Draft')
+        PUBLISHED = 'PB', _('Published')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts', verbose_name=_('Author'))
+    title = models.CharField(_('Title'), max_length=250)
+    slug = models.SlugField(_('Slug'), max_length=250, unique_for_date='publish')
+    publish = models.DateTimeField(_('Publish'), default=timezone.now)
+    created = models.DateTimeField(_('Created'), auto_now_add=True)
+    updated = models.DateTimeField(_('Updated'), auto_now=True)
+    status = models.CharField(_('Status'), max_length=2, choices=Status.choices, default=Status.DRAFT)
+    body = models.TextField(_('Body'))
+    # manager
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        verbose_name = _('Post')
+        verbose_name_plural = _('Posts')
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    def __str__(self):
+        return self.title
 
