c28348481f9ebb9c157de622098950ab9f28e1f9
Anik Ronjon <anik35-497@diu.edu.bd>
2023-02-21 00:09:20 +0600

    start newapp blog with configuration

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('travel.urls', namespace='travel')),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
 
 if settings.DEBUG:
