bb38947c10fc4459ffb520c136c864f61625d8bb
Anik Ronjon <anik35-497@diu.edu.bd>
2023-02-25 21:39:21 +0600

    home page created

--- backend/travel/urls.py
+++ backend/travel/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'travel'
 urlpatterns = [
-
+    path('', views.index_view, name='index'),
+    path('place/', views.PlaceListView.as_view(), name='place_list'),
+    path('place/<slug:name>/', views.PlaceDetailView.as_view(), name='place_detail'),
 ]
--- backend/travel/views.py
+++ backend/travel/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.conf import settings
+from .models import Category, Location, Media, Place
+from django.views.generic import ListView, DetailView
+
 
 # Create your views here.
+def index_view(request):
+    places = Place.objects.select_related('category', 'location')
+    return render(request, 'travel/index.html', {'places': places})
+
+
+class PlaceListView(ListView):
+    model = Place
+
+
+class PlaceDetailView(DetailView):
+    model = Place
