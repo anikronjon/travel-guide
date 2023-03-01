12b05b3cc57246860a42417bf5ed03eef4ec5b21
Anik Ronjon <anik35-497@diu.edu.bd>
2023-03-02 03:15:36 +0600

    bug fix: 404 error solve

--- backend/core/settings/prod.py
+++ backend/core/settings/prod.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['anikronjon.us', 'anikronjon.com', '127.0.0.1', 'localhost', '518.234.111.221']
+ALLOWED_HOSTS = ['anikronjon.us', 'anikronjon.com', '127.0.0.1', 'localhost', '18.234.111.221']
 
 
 ADMINS = [
