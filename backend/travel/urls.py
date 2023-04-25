8ad3ccfd6536ea436dfbb23fb67138d706c1531c
anikronjon <anikronjon.swe@gmail.com>
2023-04-25 11:09:39 +0600

    restructure and place, media models created

--- backend/travel/urls.py
+++ backend/travel/urls.py
@@ -4,7 +4,4 @@ from . import views
 
 app_name = 'travel'
 urlpatterns = [
-    path('', views.index_view, name='index'),
-    path('place/', views.PlaceListView.as_view(), name='place_list'),
-    path('place/<slug:name>/', views.PlaceDetailView.as_view(), name='place_detail'),
 ]
