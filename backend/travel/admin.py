bbe3b93293427aeeb1b66ea2e8f09e2747ee8139
anikronjon <anikronjon.swe@gmail.com>
2023-05-06 19:21:23 +0600

    pack travel model

--- backend/travel/admin.py
+++ backend/travel/admin.py
@@ -1,6 +1,41 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 from django.db.models import Count
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
+from .models import *
 
+
+class MediaImageInline(GenericTabularInline):
+    model = MediaImage
+
+
+class MediaVideoInline(GenericTabularInline):
+    model = MediaVideo
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(TouristPlace)
+class TouristPlaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    inlines = [MediaImageInline, MediaVideoInline]
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_description')
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_description')
+
+
+@admin.register(Agency)
+class AgencyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_description')
 
--- backend/travel/models/__init__.py
+++ backend/travel/models/__init__.py
@@ -1,8 +1,9 @@
 from .media import MediaImage, MediaVideo
 from .places import Category, TouristPlace
-
+from .feature import Hotel, Restaurant, Agency
 
 __all__ = [
     'MediaImage', 'MediaVideo',
     'Category', 'TouristPlace',
+    'Hotel', 'Restaurant', 'Agency',
 ]
