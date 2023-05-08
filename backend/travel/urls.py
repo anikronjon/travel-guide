55c26fba58f488403272126c94eabf38eb464f3c
anikronjon <anikronjon.swe@gmail.com>
2023-05-08 21:41:47 +0600

    tourist places base file

--- backend/travel/urls.py
+++ backend/travel/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 app_name = 'travel'
 urlpatterns = [
+    path('', views.home, name='home'),
 ]
--- backend/travel/views.py
+++ backend/travel/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic import ListView, DetailView
+from .models import *
 
+
+class TourListView(ListView):
+    model = TouristPlace
+
+
+class HotelListView(ListView):
+    model = Hotel
+
+
+class RestaurantListView(ListView):
+    model = Restaurant
+
+
+def home(request):
+    tour = TouristPlace.objects.all()
+    hotels = Hotel.objects.all()
+    restaurants = Restaurant.objects.all()
+    agencies = Agency.objects.all()
+    return render(request, "travel/index.html")
